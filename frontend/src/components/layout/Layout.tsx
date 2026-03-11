102f8ea5932d9cd8cbe2cc8bc532091ba4f9b26d
Roshani Sahu <roshani032003@gmail.com>
2026-03-11 19:04:44 +0530

    commit

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -21,7 +21,7 @@ const Layout: React.FC = () => {
 
       <div className={`flex flex-1 flex-col transition-all duration-300 ${collapsed ? "lg:pl-20" : "lg:pl-72"}`}>
         <Header onMenuClick={() => setSidebarOpen((p) => !p)} />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-6">
           <Outlet />
         </main>
       </div>
