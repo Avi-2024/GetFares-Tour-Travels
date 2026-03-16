9cf260434671fad17de5bd8e9346d9968ae34437
amishadubey44 <amishadubey44@gmail.com>
2026-03-16 12:44:53 +0530

    Update RefundsPage.tsx

--- frontend/src/pages/refunds/RefundsPage.tsx
+++ frontend/src/pages/refunds/RefundsPage.tsx
@@ -725,7 +725,7 @@ const RefundsPage = () => {
   }
 
   return (
-    <div className='space-y-4 sm:space-y-6 px-4 sm:px-0 max-w-7xl mx-auto'>
+    <div className='space-y-4 sm:space-y-6 px-0 sm:px-0 max-w-9xl mx-auto'>
       {/* Toast */}
       {toast.show && (
         <Toast
