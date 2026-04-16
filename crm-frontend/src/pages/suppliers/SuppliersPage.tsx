5840da9c2d495a77f992b61255b78c9f8551d966
Shivsagar <Shivsagarstd77@gmail.com>
2026-04-16 13:03:51 +0530

    Updated

--- crm-frontend/src/pages/suppliers/SuppliersPage.tsx
+++ crm-frontend/src/pages/suppliers/SuppliersPage.tsx
@@ -884,7 +884,6 @@ const SuppliersPage: React.FC = () => {
                       onChange={e => setSupplierForm(prev => ({ ...prev, email: e.target.value }))}
                       className='field-input'
                       placeholder='Enter email address'
-                      required
                     />
                   </div>
                   <div>
